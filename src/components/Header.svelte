cf87110169bb9adf85d10771887750d9fe1d2da8
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-17 10:38:48 -0300

    feat: add Avatar on Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,9 +2,9 @@
 	const svgUrl = '/logo.svg';
 </script>
 
-<header class="sticky inset-0 py-4 px-12 bg-background/80 backdrop-blur-md flex justify-between items-center">
+<header class="sticky top-0 left-0 py-4 lg:px-12 px-4 bg-background/80 backdrop-blur-md flex justify-between items-center">
 	<img src={svgUrl} alt="Discloud Logo" width="170" />
-  <nav class="">
+  <nav class="lg:flex items-center gap-4 hidden ">
     <ul class="flex gap-6">
       <li class="hover:text-primary transition-all">
         <a href="/">
@@ -25,6 +25,11 @@
         <a href="/">
         Documentação
         </a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <img src="https://github.com/neopromic.png" alt="Avatar" class="size-10 rounded-full cursor-pointer" />
       </li>
     </ul>
   </nav>
