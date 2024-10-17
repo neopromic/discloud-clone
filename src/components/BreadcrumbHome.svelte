c0740ac03e639f7c282e22509fe698d2f097eb1b
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-17 13:04:19 -0300

    feat: create Breadcrumb section

---
+++ src/components/BreadcrumbHome.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	// @ts-nocheck
+
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index';
+</script>
+
+<div>
+	<Breadcrumb.Root>
+		<Breadcrumb.List>
+      <Breadcrumb.Item>
+        <Breadcrumb.Link href="/" class="text-foreground">Inicio</Breadcrumb.Link>
+      </Breadcrumb.Item>
+      <Breadcrumb.Separator />
+      <Breadcrumb.Item>
+        <Breadcrumb.Link href="/">Painel de Controle</Breadcrumb.Link>
+      </Breadcrumb.Item>
+    </Breadcrumb.List>
+	</Breadcrumb.Root>
+</div>
