482e01d8a308751cc413616c81685382baee23ee
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-17 13:53:44 -0300

    feat: improve user infos section most complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
 	import BreadcrumbHome from '../components/BreadcrumbHome.svelte';
 	import Header from '../components/Header.svelte';
-	import { Plus } from "lucide-svelte"
-
+	import { Plus, User, Package, Github } from 'lucide-svelte';
 </script>
 
 <main class="h-[calc(100dvh-3rem)]">
@@ -10,18 +9,33 @@
 	<section class="px-4 lg:px-12 space-y-4">
 		<BreadcrumbHome />
 		<div class="flex justify-between">
-
 			<h2 class="text-2xl">Workspace</h2>
-			<button class="uppercase text-lg bg-[#4f5ad9] rounded-md flex gap-2 items-center px-2">
+			<button
+				class="uppercase text-lg bg-[#4f5ad9] rounded-md flex gap-2 items-center px-2 font-bold"
+			>
 				<Plus size={18} strokeWidth={1.5} />
 				Add App
 			</button>
 		</div>
 		<section class="rounded-md bg-card min-h-64 p-4">
 			<div class="flex justify-between">
-				<img src="https://github.com/neopromic.png" alt="Avatar" class="size-24 rounded-full">
-				<div class="flex-1">
+				<img src="https://github.com/neopromic.png" alt="Avatar" class="size-24 rounded-full" />
+				<div class="flex-1 px-6 items-center">
 					<p class="text-2xl font-bold">neopromic</p>
+					<p class="text-muted-foreground flex gap-2 items-center font-semibold">
+						<User size={16} class="fill-muted-foreground" /> ID: 621323075614408711
+					</p>
+					<p class="text-stone-700/70 flex gap-2 items-center font-semibold">
+						<Github size={16} class="fill-stone-700/70" /> 65511208
+					</p>
+				</div>
+				<div class="text-muted-foreground">
+					<p class="font-semibold text-white flex gap-2 items-center pointer-events-none select-none	">
+						<Package size={16} />
+						Recursos
+					</p>
+					<p>100 MB RAM</p>
+					<p>0.25 vCPUs</p>
 				</div>
 			</div>
 		</section>
