21ab71b4b8b7718119e321872a5c97c0fcb1ba93
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-17 00:18:33 -0300

    feat: create Header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	const svgUrl = '/logo.svg';
+</script>
+
+<header class="sticky inset-0 py-4 px-12 bg-background/80 backdrop-blur-md flex justify-between items-center">
+	<img src={svgUrl} alt="Discloud Logo" width="170" />
+  <nav class="">
+    <ul class="flex gap-6">
+      <li class="hover:text-primary transition-all">
+        <a href="/">
+        Planos
+        </a>
+      </li>
+      <li class="hover:text-primary transition-all">
+        <a href="/">
+        Serviços
+        </a>
+      </li>
+      <li class="hover:text-primary transition-all">
+        <a href="/">
+        Downloads
+        </a>
+      </li>
+      <li class="hover:text-primary transition-all">
+        <a href="/">
+        Documentação
+        </a>
+      </li>
+    </ul>
+  </nav>
+</header>
