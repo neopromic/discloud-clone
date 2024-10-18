84594c53aad903eefc5005b6a29c42ab561d9e6f
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-10-17 22:42:01 -0300

    feat: improving ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,17 +12,19 @@
 		<div class="flex justify-between">
 			<h2 class="text-2xl">Workspace</h2>
 			<button
-				class="uppercase text-lg bg-[#4f5ad9] rounded-md flex gap-2 items-center px-2 font-bold"
+				class="uppercase text-lg bg-[#4f5ad9] hover:bg-[#4f5ad9]/80 rounded-md flex gap-2 items-center px-2 font-bold"
 			>
 				<Plus size={18} strokeWidth={1.5} />
 				Add App
 			</button>
 		</div>
-		<section class="rounded-md bg-card min-h-64 p-4">
-			<div class="flex-col lg:flex-row justify-between">
+		<section class="rounded-md bg-card">
+			<div
+				class="flex lg:flex-row flex-col justify-center items-center lg:justify-between p-4 space-y-4"
+			>
 				<img src="https://github.com/neopromic.png" alt="Avatar" class="size-24 rounded-full" />
-				<div class="flex-1 px-6 items-center">
-					<p class="text-2xl font-bold">neopromic</p>
+				<div class="flex-1 px-6 items-center justify-center">
+					<p class="text-2xl font-bold text-center lg:text-start">neopromic</p>
 					<p class="text-muted-foreground flex gap-2 items-center font-semibold">
 						<User size={16} class="fill-muted-foreground" /> ID: 621323075614408711
 					</p>
@@ -42,7 +44,7 @@
 					<p>0.25 vCPUs</p>
 				</div>
 			</div>
-			<div class="relative">
+			<div class="flex-1 bg-card-darker h-full">
 				<Tabs />
 			</div>
 		</section>
