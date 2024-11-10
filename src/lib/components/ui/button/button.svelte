43b7cc7c575cce913a3c08efa67f51cf9d9aace7
Wesley Souza | Neo <cusaowesley1@gmail.com>
2024-11-10 17:47:21 -0300

    feat: :sparkles: add error boundary global

---
+++ src/lib/components/ui/button/button.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import { buttonVariants, type ButtonVariants } from './variants';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	type $$Props = HTMLButtonAttributes & {
+		variant?: ButtonVariants;
+		size?: 'default' | 'sm' | 'lg' | 'icon';
+	};
+
+	let className: $$Props['class'] = undefined;
+	export let variant: $$Props['variant'] = 'default';
+	export let size: $$Props['size'] = 'default';
+	export { className as class };
+</script>
+
+<button class={cn(buttonVariants({ variant, size, className }))} {...$$restProps} on:click>
+	<slot />
+</button>
